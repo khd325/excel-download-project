bda80654fb65fa7278e295e22d4efa60a2d5ef80
khd325 <hdk325@naver.com>
2023-08-13 16:38:01 +0900

    cellStyle 적용

--- src/main/java/com/example/excel/controller/TestController.java
+++ src/main/java/com/example/excel/controller/TestController.java
@@ -1,15 +1,16 @@
 package com.example.excel.controller;
 
 import com.example.excel.dto.TestDto;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.awt.Color;
 import java.io.IOException;
 import java.util.List;
 
@@ -20,6 +21,12 @@ public class TestController {
     public void downloadExcel(HttpServletResponse response) throws IOException {
         Workbook workbook = new SXSSFWorkbook();
 
+        CellStyle greyCellStyle = workbook.createCellStyle();
+        applyCellStyle(greyCellStyle, new Color(231,234,236));
+
+        CellStyle bodyCellStyle = workbook.createCellStyle();
+        applyCellStyle(bodyCellStyle, new Color(255,255,255));
+
         Sheet sheet = workbook.createSheet();
 
         List<TestDto> testDtos = TestDto.makeDummyData();
@@ -28,18 +35,22 @@ public class TestController {
         Row headerRow = sheet.createRow(rowIndex++);
 
         Cell headerCell = headerRow.createCell(0);
+        headerCell.setCellStyle(greyCellStyle);
         headerCell.setCellValue("이름");
 
         headerCell = headerRow.createCell(1);
+        headerCell.setCellStyle(greyCellStyle);
         headerCell.setCellValue("나이");
 
         for (TestDto testDto : testDtos) {
             Row bodyRow = sheet.createRow(rowIndex++);
 
             Cell bodyCell = bodyRow.createCell(0);
+            bodyCell.setCellStyle(bodyCellStyle);
             bodyCell.setCellValue(testDto.getName());
 
             bodyCell = bodyRow.createCell(1);
+            bodyCell.setCellStyle(bodyCellStyle);
             bodyCell.setCellValue(testDto.getAge());
         }
 
@@ -47,7 +58,18 @@ public class TestController {
 
         workbook.write(response.getOutputStream());
         workbook.close();
+    }
 
 
+    private void applyCellStyle(CellStyle cellStyle, java.awt.Color color) {
+        XSSFCellStyle xssfCellStyle = (XSSFCellStyle) cellStyle;
+        xssfCellStyle.setFillForegroundColor(new XSSFColor(color, new DefaultIndexedColorMap()));
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+        cellStyle.setBorderBottom(BorderStyle.THIN);
     }
 }
